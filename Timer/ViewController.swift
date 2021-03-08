9342b019e42d3af6fa7fa5749b522cb0bda575c1
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-03-08 16:46:06 +0900

    実装1

--- Timer/ViewController.swift
+++ Timer/ViewController.swift
@@ -7,8 +7,11 @@
 //
 
 import UIKit
+import AudioToolbox
 
 class ViewController: UIViewController {
+    var timer : Timer?   //Timerインスタンスの保持
+    var currentSeconds = 0  //タイマーの設定時間と経過時間の差分
 
     @IBOutlet weak var label: UILabel!
     
@@ -19,12 +22,36 @@ class ViewController: UIViewController {
 
 
     @IBAction func tenSecButtonTapped(_ sender: UIButton) {
+        start(seconds: 10)
     }
     
     @IBAction func threeMinButtonTapped(_ sender: UIButton) {
+        start(seconds: 180)
     }
     
     @IBAction func fiveMinButtonTapped(_ sender: UIButton) {
+        start(seconds: 300)
     }
+    
+    func start(seconds: Int){
+        currentSeconds = seconds
+        label.text = "残り\(currentSeconds)秒"
+        timer = Timer.scheduledTimer(timeInterval: 1.0,
+                                           target: self,
+                                           selector: #selector(ViewController.update),
+                                           userInfo: nil,
+                                           repeats: true)
+     } //タイマーを開始するメソッド
+    
+    @objc func update(){
+        currentSeconds -= 1
+        label.text = "残り\(currentSeconds)秒"
+        if (currentSeconds == 0){
+            timer?.invalidate()
+            let soundId: SystemSoundID = 1005
+            AudioServicesPlayAlertSound(soundId)
+        }
+    } //更新するメソッド
+    
 }
 
