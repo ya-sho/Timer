15638a5d3623354be302c672ede15163a6d93a7a
ya-sho <78021007+ya-sho@users.noreply.github.com>
2021-03-08 11:11:42 +0900

    Initial Commit

---
+++ Timer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Timer
+//
+//  Created by Shogo Yamamoto on 2021/03/08.
+//  Copyright © 2021年 めんつゆ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
